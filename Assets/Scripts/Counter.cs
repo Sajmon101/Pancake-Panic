5694ae60b532c78f883cef1fa921f23e22c49e7f
Sajmon101 <szymk4@wp.pl>
2024-05-30 22:38:52 +0200

    Added kitchen scriptical objects

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
+
+    [SerializeField] KitchenObjectSO kitchenObjectSO;
+    [SerializeField] Transform CounterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform kitchenObjectSOTransform = Instantiate(kitchenObjectSO.prefab.transform, CounterTopPoint);
+        kitchenObjectSOTransform.localPosition = Vector3.zero;
+        Debug.Log(kitchenObjectSOTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+
     }
 }
